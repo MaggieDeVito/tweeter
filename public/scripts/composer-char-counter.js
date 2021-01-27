6c2f7ed4de5762ead4f67d6ba504131f0c801365
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-27 00:13:40 +0000

    created an event handler for the counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#tweet-text").on('input', function() {
+    const $input = $(this);
+    const form = $input.closest('form');
+    const counter = form.find('.counter');
+    const charsLeft = (140 - $input.val().length);
+
+    counter.html(charsLeft);
+
+    if(charsLeft < 0) {
+      counter.addClass("addColour")
+    } else {
+      counter.removeClass("addColour")
+    }
+  })
+});
